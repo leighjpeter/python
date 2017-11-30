340f3fd2ecef884e643ec6b0abc0032fbbe59e53
leighj <leighj@163.com>
2017-11-30 17:38:35 +0800

    日常

--- datetime_demo.py
+++ datetime_demo.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# timestamp是一个浮点数，它没有时区的概念，而datetime是有时区的
 from datetime import datetime
+
+i = datetime.now()
+print ("当前的日期和时间是 %s" % i)
+print ("ISO格式的日期和时间是 %s" % i.isoformat() )
+print ("当前的年份是 %s" %i.year)
+print ("当前的月份是 %s" %i.month)
+print ("当前的日期是  %s" %i.day)
+print ("dd/mm/yyyy 格式是  %s/%s/%s" % (i.day, i.month, i.year) )
+print ("当前小时是 %s" %i.hour)
+print ("当前分钟是 %s" %i.minute)
+print ("当前秒是  %s" %i.second)
+
+
 
 # 创建时间
 dt = datetime(2017,11,21,16,21)
@@ -12,18 +26,40 @@ dt.timestamp() # 1511281260.0
 # timestamp转换为datetime
 t = 1511281260.0
 datetime.fromtimestamp(t)
+datetime.utcfromtimestamp(t)
 # str转换为datetime
 datetime.strptime('2017-11-21 16:28:00','%Y-%m-%d %H:%M:%S')
 # datetime转换为str
 now = datetime.now()
 now.strftime('%a, %b %d %H:%M')
 
-
+# 使用timedelta加减datetime
+now = datetime.now()
+now + timedelta(hours=10)
+now - timedelta(days=1)
+now + timedelta(days=2, hours=12)
 
 # 本地时间转换为UTC时间
 # 一个datetime类型有一个时区属性tzinfo，但是默认为None，所以无法区分这个datetime到底是哪个时区
 
-from datetime import datetime, timedelta, timezone
-tz_utc_8 = timezone(timedelta(hours=8))
+# 前提是系统时区恰好是UTC+8:00
+# from datetime import datetime, timedelta, timezone
+# tz_utc_8 = timezone(timedelta(hours=8))
+# now = datetime.now()
+# print(now)
+# dt = now.replace(tzinfo=tz_utc_8)
+# print(dt)
+
+# 时区转换
+# 通过utcnow拿到当前时间，再转换
+from datetime import datetime,timezone,timedelta
+utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
+print(utc_dt) # 2017-11-30 09:12:04.608029+00:00
 now = datetime.now()
-print(now)
+print(now) # 2017-11-30 09:12:04.608646
+
+# astimezone()将转换时区为北京时间:
+bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
+print(bj_dt) # 2017-11-30 17:13:52.826238+08:00
+
+print(bj_dt.strftime('%Y-%m-%d'))
--- re_match.py
+++ re_match.py
@@ -5,25 +5,25 @@
 # 
 # 
 import re
-re.match()
+# re.match()
 # 切分字符串
-re.split(r'[\s\,]+', 'a,b, c  d')
+# re.split(r'[\s\,]+', 'a,b, c  d')
 # 分组
-re.group()
+# re.group()
 # 贪婪匹配
-re.match(r'^(\d+?)(0*)$', '102300').groups() #('1023', '00')
+# re.match(r'^(\d+?)(0*)$', '102300').groups() #('1023', '00')
 
 # 编译
 re_tel = re.compile(r'^(\d{3})-(\d{3,8})$')
 re_tel.match('001-121').groups()
 
 # 作业一验证邮箱
-re_email = re.compile(r'^[a-zA-Z\.]+@[a-zA-Z0-9]+\.[a-zA-Z]{3}$')
-def regEmail(email):
-	if(re_email.match(email)):
-		print('%s is a correct email address' %email)
-	else:
-		print('%s is a wrong email!!!!!' % email)
+# re_email = re.compile(r'^[a-zA-Z\.]+@[a-zA-Z0-9]+\.[a-zA-Z]{3}$')
+# def regEmail(email):
+# 	if(re_email.match(email)):
+# 		print('%s is a correct email address' %email)
+# 	else:
+# 		print('%s is a wrong email!!!!!' % email)
 
-regEmail('someone@gmail.com')
-regEmail('bill.gates@microsoft.com')
+# regEmail('someone@gmail.com')
+# regEmail('bill.gates@microsoft.com')
