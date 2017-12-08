b335e855867e115905bd170bcd4e5e5a8e675435
leighj <leighj@163.com>
2017-12-08 18:28:10 +0800

    read file

---
+++ read_file.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# txt
+from urllib.request import urlopen
+# textPage = urlopen('http://www.pythonscraping.com/pages/warandpeace/chapter1.txt')
+# print(textPage.read())
+
+# csv
+from io import StringIO
+import csv
+# data = urlopen('http://pythonscraping.com/files/MontyPythonAlbums.csv').read().decode('ascii',errors='ignore')
+# dataFile = StringIO(data)
+# csvReader = csv.reader(dataFile)
+# for row in csvReader:
+	# print(row)
+
+# dictReader = csv.DictReader(dataFile)	 # ['Name', 'Year']
+# print(dictReader.fieldnames)
+# for row in dictReader:
+# 	print(row)		# OrderedDict([('Name', "Monty Python's Flying Circus"), ('Year', '1970')])
+
+# word
+# 
+from zipfile import ZipFile
+from urllib.request import urlopen
+from io import BytesIO
+from bs4 import BeautifulSoup
+
+wordFile = urlopen("http://pythonscraping.com/pages/AWordDocument.docx").read()
+wordFile = BytesIO(wordFile)
+document = ZipFile(wordFile)
+xml_content = document.read('word/document.xml')
+# print(xml_content.decode('utf-8'))
+wordObj = BeautifulSoup(xml_content.decode('utf-8'),"lxml")
+# print(type(wordObj))
+textStrings = wordObj.findAll("w:t")
+# print(textStrings)
+for textElem in textStrings:
+	print(textElem.text)
